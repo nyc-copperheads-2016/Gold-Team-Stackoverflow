883253f9a50efc9df6b0f0b7f785f0f5f4545f25
Ryan Lesson & Christopher Lee & Jonathan Chen <pair+bigless27.cklee8.dijonkitchen@devbootcamp.com>
2015-12-31 17:07:10 -0500

    add spec helpers

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,4 +3,10 @@ require 'rails_helper'
 RSpec.describe Vote, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 
+  describe 'voter' do
+    it 'A vote can call voter on it to see the user association' do
+      ryan=Vote.new(up_vote: false)
+      expect(ryan.voter).to be_defined
+   end
+  end
 end
