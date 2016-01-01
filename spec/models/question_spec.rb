bdae3e79d0a9241b44c59536e4c45af51967d97e
Ryan Lesson <email": null>
2016-01-01 17:25:55 -0500

    Add factory girl

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
+before(:each) do
+  @question= FactoryGirl.create(:comment_question)
+end
+
+before(:each) do
+  @vote= FactoryGirl.create(:vote_answer)
+end
+
 RSpec.describe Question, type: :model do
   "add some examples to (or delete) #{__FILE__}"
-
-  describe 'polymorphism works for the questions model' do
 
 end
