49f5dd86b2a4362b7152dd25bff7ef8d41c11edc
Ryan Lesson <email": null>
2016-01-01 15:31:06 -0500

    Fix model associations and remove pending on Rspec

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :user
   has_many :comments, as: :commentable
+  has_many :votes, as: :votable
   has_many :answers
   has_many :votes
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  "add some examples to (or delete) #{__FILE__}"
 
   describe 'voter' do
     it 'A vote can call user on it to see the user association' do
