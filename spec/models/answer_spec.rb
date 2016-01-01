60ea1b8a46acebaccbefba3de18d17b32a0fceda
Ryan Lesson <email": null>
2016-01-01 17:36:14 -0500

    fix before(:each) association bug

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
-before(:each) do
-  @question= FactoryGirl.create(:comment_answer)
-end
 
-before(:each) do
-  @vote= FactoryGirl.create(:vote_answer)
-end
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @question= FactoryGirl.create(:comment_answer)
+  end
+
+  before(:each) do
+    @vote= FactoryGirl.create(:vote_answer)
+  end
+
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 
-before(:each) do
-  @question= FactoryGirl.create(:comment_question)
-end
 
-before(:each) do
-  @vote= FactoryGirl.create(:vote_answer)
-end
 
 RSpec.describe Question, type: :model do
-  "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @question= FactoryGirl.create(:comment_question)
+  end
+
+  before(:each) do
+    @vote= FactoryGirl.create(:vote_answer)
+  end
 
   describe 'polymorphism works for the questions model' do
   end
+
 
 end
