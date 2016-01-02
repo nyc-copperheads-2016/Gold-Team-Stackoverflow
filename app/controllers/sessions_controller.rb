214bc1e4d842c20b88b2e7716b07ad1a67d5591f
Christopher Lee <email": null>
2016-01-02 18:56:40 -0500

    finish rebase

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
-	
-	def new
-	end
+  def new
+  end
 
+<<<<<<< 8baf3ffff4fbca3636d09c183622e37f4d74b0a7
 	def create
 		user = User.find_by(username: params[:username])
 		if user && user.authenticate(params[:password])
@@ -17,4 +17,20 @@ class SessionsController < ApplicationController
 		session[:user_id] = nil
 		redirect_to new_session_path
 	end
+=======
+  def create
+    user = User.new(username: params[:username], password: params[:password])
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to new_user_path
+  end
+>>>>>>> tweak sessions controller
 end
