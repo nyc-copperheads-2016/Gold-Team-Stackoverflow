b97bb1526322ca49442bafcecbe1fca73ca24f70
Ryan Lesson <email": null>
2016-01-02 15:15:52 -0500

    Add routes to Answers controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,21 +1,51 @@
 class AnswersController < ApplicationController
 
   def index
+    @answers= Answer.all
   end
 
   def new
+    @question=Answer.new
   end
 
   def create
+    @answer=Answer.new(answer_params)
+    if @answer.save
+      redirect_to :root
+    else
+      render :new
+    end
+  end
+
+
+  def show
+    @answer=Answer.find(params[:id])
   end
 
   def edit
+    @answer=Answer.find(params[:id])
   end
 
   def update
+    @answer=Answer.find(params[:id])
+    if @answer.update_attributes(answer_params)
+      render_to @answer
+    else
+      render :edit
+    end
   end
 
   def destroy
+    answer=Answer.find(params[:id])
+    answer.delete
+
+    render :index
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:content, :favorite)
   end
 
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,8 +8,8 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    question = Question.new(question_params)
-    if question.save
+    @question = Question.new(question_params)
+    if @question.save
       redirect_to :root
     else
       render :new
@@ -22,13 +22,12 @@ class QuestionsController < ApplicationController
 
   def edit
     @question = Question.find(params[:id])
-
   end
 
   def update
-    question = Question.find(params[:id])
-    if question.update_attributes(question_params)
-      redirect_to question
+    @question = Question.find(params[:id])
+    if @question.update_attributes(question_params)
+      redirect_to @question
     else
       render :edit
     end
