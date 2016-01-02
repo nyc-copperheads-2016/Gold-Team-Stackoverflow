f1a112bde6f9cd6494d4d22f59edaa9e2a5aed57
Christopher Lee <zerosxl@gmail.com>
2016-01-02 11:35:42 -0500

    create signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,22 @@
 class UsersController < ApplicationController
 
+  def new
+    @new_user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to "/"
+    else
+      redirect_to '/users/new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
    resources :answers, except: [:index]
   end
   resources :sessions, only: [:create, :destroy, :new]
+
+  resources :users, only: [:new, :create, :show]
 end
