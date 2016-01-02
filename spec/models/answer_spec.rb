ebb1d4e39db725c8d0440eedbed8a5c1c2847e7f
Ryan Lesson <email": null>
2016-01-02 14:08:35 -0500

    Add Answer spec tests

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,8 +3,17 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
 
   before(:each) do
+    @answer=FactoryGirl.create(:answer)
     @comment= FactoryGirl.create(:comment_answer)
     @vote= FactoryGirl.create(:vote_answer)
+  end
+
+  describe Answer, type: :model do
+
+    it {should belong_to(:question)}
+    it {should belong_to (:user)}
+    it {should have_many (:comments)}
+    it {should have_many (:votes)}
   end
 
 end
