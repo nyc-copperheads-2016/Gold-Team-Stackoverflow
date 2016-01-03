ea678426c0e7c69209a07bd34438454d768b250c
dijonkitchen <jschen2008@gmail.com>
2016-01-03 15:08:16 -0500

    Add methods for vote counts

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,20 @@
 class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers, dependent: :destroy
-  has_many :comments, as: :commentable, dependent: :destroy
   has_many :votes, as: :voteable, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
+
+  def net_votes
+    upvotes - downvotes
+  end
+
+  private
+
+  def upvotes
+    votes.where(upvote: true).count
+  end
+
+  def downvotes
+    votes.where(upvote: false).count
+  end
 end
