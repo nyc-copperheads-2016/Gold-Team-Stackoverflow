f5e1467a9283a151e812579568e4a41831791415
dijonkitchen <jschen2008@gmail.com>
2016-01-03 09:45:33 -0500

    Add Answers destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root 'questions#index'
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:create, :destroy, :new]
   resources :questions do
     resources :answers, except: [:index, :show]
   end
-  resources :sessions, only: [:create, :destroy, :new]
-
-  resources :users, only: [:new, :create, :show]
 end
