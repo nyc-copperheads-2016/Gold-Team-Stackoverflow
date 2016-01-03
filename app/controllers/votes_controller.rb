a8a525e8f5fb1a9a8bfeb33ac42ba6e8fdb27de2
dijonkitchen <jschen2008@gmail.com>
2016-01-03 17:37:30 -0500

    Add form for upvote/downvote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,19 +3,11 @@ class VotesController < ApplicationController
 
   def create
     question = Question.find(params[:question_id])
-    # For upvotes
-    vote = question.votes.find_or_initialize_by(upvote: true, user: current_user)
-    if vote.save
-      redirect_to :root
+    if params[:vote] == 'up'
+      vote = question.votes.find_or_initialize_by(upvote: true, user: current_user)
     else
-      render :root
+      vote = question.votes.find_or_initialize_by(upvote: false, user: current_user)
     end
-  end
-
-  def update
-    question = Question.find(params[:question_id])
-    # For downvotes
-    vote = question.votes.find_or_initialize_by(upvote: false, user: current_user)
     if vote.save
       redirect_to :root
     else
